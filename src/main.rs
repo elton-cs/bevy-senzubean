bd6934d268aca649d9bba4c8b99f7e6bba308a04
elton-cs <govada.elton@gmail.com>
2024-06-23 03:15:37 -0500

    Refactor torii client into bevy plugin

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,13 @@ use bevy::{
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
-use bevy_senzubean::torii::run_torii_client;
+use bevy_senzubean::{plugins::torii::ToriiPlugin, torii::run_torii_client};
 use starknet_ff::FieldElement;
 use tokio::runtime::Builder;
-use torii_grpc::types::schema::Entity as ToriiData;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup_tokio_and_torii)
-        .add_systems(Update, update_torii_data)
+        .add_plugins(ToriiPlugin)
         .run();
-}
-
-#[derive(Resource)]
-struct ToriiResource {
-    entity: ToriiData,
-    rx: tokio::sync::mpsc::Receiver<ToriiData>,
-}
-
-fn setup_tokio_and_torii(mut commands: Commands) {
-    // run torii client in separate thread via tokio
-    let tokio_runtime = Builder::new_current_thread()
-        .worker_threads(1)
-        .enable_all()
-        .build()
-        .unwrap();
-
-    let (tx, rx) = tokio::sync::mpsc::channel::<ToriiData>(16);
-
-    std::thread::spawn(move || {
-        tokio_runtime.block_on(run_torii_client(tx));
-    });
-
-    let default_entity = ToriiData {
-        hashed_keys: FieldElement::default(),
-        models: Vec::new(),
-    };
-
-    commands.insert_resource(ToriiResource {
-        entity: default_entity,
-        rx,
-    });
-}
-
-fn update_torii_data(mut torii_entity: ResMut<ToriiResource>) {
-    if let Ok(new_entity) = torii_entity.rx.try_recv() {
-        info!("Message from Torii Client: {:?}", new_entity);
-        torii_entity.entity = new_entity;
-    }
 }
--- src/plugins/torii.rs
+++ src/plugins/torii.rs
@@ -1,1 +1,51 @@
+use crate::torii::run_torii_client;
+use bevy::prelude::*;
+use starknet_ff::FieldElement;
+use tokio::runtime::Builder;
+use torii_grpc::types::schema::Entity as ToriiData;
 
+pub struct ToriiPlugin;
+impl Plugin for ToriiPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup_tokio_and_torii)
+            .add_systems(Update, update_torii_data);
+    }
+}
+
+#[derive(Resource)]
+struct ToriiResource {
+    entity: ToriiData,
+    rx: tokio::sync::mpsc::Receiver<ToriiData>,
+}
+
+fn setup_tokio_and_torii(mut commands: Commands) {
+    // run torii client in separate thread via tokio
+    let tokio_runtime = Builder::new_current_thread()
+        .worker_threads(1)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let (tx, rx) = tokio::sync::mpsc::channel::<ToriiData>(16);
+
+    std::thread::spawn(move || {
+        tokio_runtime.block_on(run_torii_client(tx));
+    });
+
+    let default_entity = ToriiData {
+        hashed_keys: FieldElement::default(),
+        models: Vec::new(),
+    };
+
+    commands.insert_resource(ToriiResource {
+        entity: default_entity,
+        rx,
+    });
+}
+
+fn update_torii_data(mut torii_entity: ResMut<ToriiResource>) {
+    if let Ok(new_entity) = torii_entity.rx.try_recv() {
+        info!("Message from Torii Client: {:?}", new_entity);
+        torii_entity.entity = new_entity;
+    }
+}
